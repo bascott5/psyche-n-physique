8177495f181a53ae8599a85814f846eeb3d89c70
bascott5 <bascott1002@gmail.com>
2024-04-16 23:55:06 -0400

    Added toggle button

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 import nextMDX from '@next/mdx'
 import rehypeSlug from 'rehype-slug'
-import rehypeSanitize from 'rehype-sanitize'
 
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
   options: {
-    rehypePlugins: [rehypeSlug, rehypeSanitize],
+    rehypePlugins: [rehypeSlug],
   },
 })
 
--- src/components/nav-menu.js
+++ src/components/nav-menu.js
@@ -1,10 +1,14 @@
 "use client";
 import { useWindowSize } from "@uidotdev/usehooks";
 import { useState, useEffect } from "react";
+import Image from "next/image";
 
 const NavMenu = ({ children }) => {
-    const [open, isOpen] = useState(false);
+    // TODO: insert table of contents into markdown itself, using a list to order everything and create a container
+
     const window = useWindowSize();
+    const [open, isOpen] = useState(true);
+    const toggle = () => isOpen(!open);
 
     useEffect(() => {
         isOpen(window.width >= 1500 ? true : false);
@@ -12,25 +16,26 @@ const NavMenu = ({ children }) => {
 
     return (
         <div className="flex basis-100% flex-col flex-1 flex-wrap">
-            <div style={{ visibility: open ? "visible" : "hidden" }} className="bg-neutral-100 fixed h-100px top-0 left-0 bottom-0 overflow-y-scroll border-r-2 border-grey">
+            <Image width={25} height={25} style={{ zIndex: "1" }} className="fixed top-5 left-5" onClick={toggle} />
+            <div style={{ visibility: open ? "visible" : "hidden" }} className="bg-neutral-100 fixed top-0 left-0 bottom-0 overflow-y-scroll border-r-2 border-grey">
                 <ul className="relative top-10">
-                    <a className="block" href={`#user-content-what-is-this-game`}><li className="list-disk">What is this game?</li></a>
-                    <a className="block" href={`#user-content-checks`}><li className="list-disk">Checks</li></a>
-                    <a className="block" href={`#user-content-challenges`}><li className="list-disk">Challenges</li></a>
-                    <a className="block" href={`#user-content-psyche`}><li className="list-disk">Psyche</li></a>
-                    <a className="block" href={`#user-content-physique`}><li className="list-disk">Physique</li></a>
-                    <a className="block" href={`#user-content-body-and-mind`}><li className="list-disk">Body and Mind</li></a>
-                    <a className="block" href={`#user-content-attributes`}><li className="list-disk">Attributes</li></a>
-                    <a className="block" href={`#user-content-abilities`}><li className="list-disk">Abilities</li></a>
-                    <a className="block" href={`#user-content-skills`}><li className="list-disk">Skills</li></a>
-                    <a className="block" href={`#user-content-inventory`}><li className="list-disk">Inventory</li></a>
-                    <a className="block" href={`#user-content-death`}><li className="list-disk">Death</li></a>
-                    <a className="block" href={`#user-content-optional-rule-humanoids`}><li className="list-disk">Optional Rule: Humanoids</li></a>
-                    <a className="block" href={`#user-content-character-creation`}><li className="list-disk">Character Creation</li></a>
-                    <a className="block" href={`#user-content-for-the-gm`}><li className="list-disk">For the GM</li></a>
-                    <a className="block" href={`#user-content-challenge-creation`}><li className="list-disk">Challenge Creation</li></a>
-                    <a className="block" href={`#user-content-skill-list`}><li className="list-disk">Skill List</li></a>
-                    <a className="block" href={`#user-content-ability-list`}><li className="list-disk">Ability List</li></a>
+                    <a className="block" href={`#what-is-this-game`}><li>What is this game?</li></a>
+                    <a className="block" href={`#checks`}><li>Checks</li></a>
+                    <a className="block" href={`#challenges`}><li>Challenges</li></a>
+                    <a className="block" href={`#psyche`}><li>Psyche</li></a>
+                    <a className="block" href={`#physique`}><li>Physique</li></a>
+                    <a className="block" href={`#body-and-mind`}><li>Body and Mind</li></a>
+                    <a className="block" href={`#attributes`}><li>Attributes</li></a>
+                    <a className="block" href={`#abilities`}><li>Abilities</li></a>
+                    <a className="block" href={`#skills`}><li>Skills</li></a>
+                    <a className="block" href={`#inventory`}><li>Inventory</li></a>
+                    <a className="block" href={`#death`}><li>Death</li></a>
+                    <a className="block" href={`#optional-rule-humanoids`}><li>Optional Rule: Humanoids</li></a>
+                    <a className="block" href={`#character-creation`}><li>Character Creation</li></a>
+                    <a className="block" href={`#for-the-gm`}><li>For the GM</li></a>
+                    <a className="block" href={`#challenge-creation`}><li>Challenge Creation</li></a>
+                    <a className="block" href={`#skill-list`}><li>Skill List</li></a>
+                    <a className="block" href={`#ability-list`}><li>Ability List</li></a>
                 </ul>
             </div>
             <div style={open ? { marginRight: "8%", marginLeft: "auto", width: "65%" } : { marginRight: "auto", marginLeft: "auto", width: "75%" }} className="h-100px">{ children }</div>
