fb08e9982a188bad4bd93791351861699cb05127
Bennett Scott <111152324+bascott5@users.noreply.github.com>
2024-04-18 22:31:30 -0400

    Added config for Actions

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+import nextMDX from '@next/mdx'
+import rehypeSlug from 'rehype-slug'
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    rehypePlugins: [rehypeSlug],
+  },
+})
+
+export default withMDX({
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  output: 'export',
+})
